1a7a0d99db3a90fc6cafaef7b2d7ce97189facc3
Tom Horton <horton.uva@gmail.com>
2019-04-24 17:15:15 -0400

    Fixed bug on deserializing Collections

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -3,6 +3,10 @@ import java.util.List;
 import java.util.HashMap;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+
+import javax.swing.plaf.synth.SynthTextAreaUI;
 
 public class Demo {
 
@@ -20,10 +24,24 @@ public class Demo {
         List<String> slist = List.of("hi", "there", "tom");
         System.out.println( "\nserialized List<String>: " + gson.toJson(slist) );
 
+        // Update to class demo: let's try to de-serialize what toJson() creates
+        // You might think this would work:
+        // List<String> slist2 = gson.fromJson(gson.toJson(slist), slist.getClass());
+        // It will for primitive types.  But it won't for collections. Type info gets lost.
+        // GSON has a way to do this, but it uses reflection and looks quite mysterious to us.
+
+        // so instead the one line above that's commented out, that uses getClass(), do this:
+        Type listType = new TypeToken<List<String>>(){}.getType(); // get object to "store" type info about List<String>
+        List<String> slist2 = gson.fromJson(gson.toJson(slist), listType); // use that as 2nd parameter to fromJson()
+
+        System.out.println("deserialized List<String>: " + slist2);
+        System.out.println("Should be equals() to original list: " + slist2.equals(slist));
+
+
         HashMap<String,Integer> lengthMap = new HashMap<>();
         lengthMap.put("a", 1);
         lengthMap.put("hello", 5);
-        System.out.println( "serialized Map: " + gson.toJson(lengthMap) );
+        System.out.println( "\nserialized Map: " + gson.toJson(lengthMap) );
 
         HashMap<Integer,List<String>> lengthToStrings = new HashMap<>();
         lengthToStrings.put(1, List.of("a", "i"));
@@ -32,8 +50,21 @@ public class Demo {
         jsonString =  gson.toJson(lengthToStrings);
         System.out.println( "serialized Map<Integer,List<String>>: " + jsonString );
 
+
+        // Update to what we demo'd in class!  Again, getClass() with fromJson() won't work.
         HashMap<Integer,List<String>> newMap = gson.fromJson(jsonString, lengthToStrings.getClass());
-        System.out.println("deserialized Map<Integer,List<String>>:" + newMap);
+        System.out.println("\nIncorrect way:  deserialized Map<Integer,List<String>>:" + newMap);
+        System.out.println( "are they equals()? " + newMap.equals(lengthToStrings)); // will return false!  ruh-roh, Shaggy!
+        System.out.println( "But it looks like deserialized version: " + lengthToStrings ); // they kind of look the same, though
+
+        System.out.println("type stored for newMap.get(\"1\"): " + newMap.get("1")); // wait, wasn't the key supposed to an Integer?
+        System.out.println("type stored for lengthToStrings.get(1): " + lengthToStrings.get(1));
+
+        // correct way to do this using TypeToken
+        Type type = new TypeToken<HashMap<Integer,List<String>>>(){}.getType();
+        newMap = gson.fromJson(jsonString, type);
+        System.out.println("\nCorrect way: deserialized Map<Integer,List<String>>:" + newMap);
+        System.out.println( "Now should be equals(): " + newMap.equals(lengthToStrings));
 
         // serialize and deserialize our own class Course
         Course c1 = new Course("cs3240", "Adv SW Dev", List.of("sherriff", "horton"), 120);
